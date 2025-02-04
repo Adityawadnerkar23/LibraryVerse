537b09922f83521e1a6e3aa8c6a9eca4bcd34e18
Aditya Wadnerkar <129152181+Adityawadnerkar23@users.noreply.github.com>
2025-02-04 15:48:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors({
-    origin: 'https://library-verse.vercel.app/',
+    origin: 'https://library-verse.vercel.app',
     methods:['GET','PUT','POST','DELETE'],
     credentials:true
 }));
